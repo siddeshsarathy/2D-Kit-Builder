0ceacbfd1638acfea15d470a0e2af17762c8384c
siddeshsarathy <47939983+siddeshsarathy@users.noreply.github.com>
2025-02-24 01:19:12 +1100

    Update Controls.js

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Controls = ({ setKitType, setSport, setColor, setView }) => {
+const Controls = ({ setKitType, setColor, setView }) => {
   return (
     <div className="controls">
       <label>Kit Type:</label>
@@ -8,17 +8,6 @@ const Controls = ({ setKitType, setSport, setColor, setView }) => {
         <option value="Jersey">Jersey</option>
         <option value="Shorts">Shorts</option>
         <option value="Stockings">Stockings</option>
-        <option value="Polo">Polo</option>
-        <option value="Hoodie">Hoodie</option>
-      </select>
-
-      <label>Sport:</label>
-      <select onChange={(e) => setSport(e.target.value)}>
-        <option value="Soccer">Soccer</option>
-        <option value="Basketball">Basketball</option>
-        <option value="AFL">AFL</option>
-        <option value="Netball">Netball</option>
-        <option value="Cricket">Cricket</option>
       </select>
 
       <label>Color:</label>
